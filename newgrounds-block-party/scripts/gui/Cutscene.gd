46720885b7c19bfa1f30bf42b3eb9addea4bdef2
migmoog <orangej0312@gmail.com>
2022-06-10 23:37:08 -0400

    Ending cutscene done, edit to top limit of cam in level 10

---
+++ newgrounds-block-party/scripts/gui/Cutscene.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+onready var scn_changer = get_node("/root/SceneChanger")
+#onready var next_scene = preload("res://scenes/MainMenu.tscn")
+export var path_to_next = "MainMenu.tscn"
+var next_scene
+
+
+func _ready() -> void:
+	Music.stop()
+	$AnimationPlayer.play("cutscene")
+	next_scene = load("res://scenes/" + path_to_next)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		get_tree().change_scene("res://scenes/" + path_to_next)
+		
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == 'cutscene':
+		scn_changer.fade_scene(next_scene)
+#		get_tree().change_scene("res://scenes/MainMenu.tscn")
